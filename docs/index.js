6b635265dbfc86cf88d1adcb28e2c1210e3efd06
Théo.L <lecardot14@gmail.com>
2025-03-01 10:06:23 -0500

    Resize

--- docs/index.js
+++ docs/index.js
@@ -96,7 +96,7 @@ async function renderMap() {
   // Barcelone -> Beauvais
   add_flight(map, [pointBCN, pointBVA]).addTo(map).start();
 
-  map.panTo(pointYUL);
+  map.setView(pointYUL);
 
   var legend = L.control({ position: "bottomleft" });
 
